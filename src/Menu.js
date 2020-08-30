dcdba79450fc5b4089077f45e2b5c267421e09c1
carladaianasl <carladaianasl@gmail.com>
2020-08-29 22:22:05 -0300

    style.css

--- src/Menu.js
+++ src/Menu.js
@@ -1,33 +1,34 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import './style.css';
 import Home from './components/Home.js';
 import About from './components/About.js';
 import Contact from './components/Contact.js';
 
 
-class App extends Component {
+class Menu extends Component {
   render() {
     return (
     <Router>
         <div>
-          <h2>Welcome to React Router Tutorial</h2>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <ul className="navbar-nav mr-auto">
-            <li><Link to={'/'} className="nav-link"> Home </Link></li>
-            <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
-            <li><Link to={'/about'} className="nav-link">About</Link></li>
-          </ul>
+
+          <nav>
+            <ul>
+              <li><Link to={'/'} className="nav-link"> Home </Link></li>
+              <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+              <li><Link to={'/about'} className="nav-link">About</Link></li>
+            </ul>
           </nav>
-          <hr />
+          
           <Switch>
               <Route exact path='/' component={Home} />
               <Route path='/contact' component={Contact} />
               <Route path='/about' component={About} />
           </Switch>
         </div>
-      </Router>
+    </Router>
     );
   }
 }
 
-export default App;
+export default Menu;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Menu from './Menu';
 
-ReactDOM.render(<App />, document.getElementById('App'));
+ReactDOM.render(<Menu />, document.getElementById('Menu'));
