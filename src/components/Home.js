8a4b027f0c7d8596a08cae6998fff0e69b564a21
carladaianasl <carladaianasl@gmail.com>
2020-08-29 23:28:22 -0300

    até o texto no home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
+import Radium, {StyleRoot} from 'radium';
+import { fadeInDown, fadeIn } from 'react-animations';
+
+const styles = {
+	fadeInDown: {
+		animation: 'x 2s',
+		animationName: Radium.keyframes(fadeInDown, 'fadeInDown')
+	},
+
+	fadeIn: {
+		animation: 'x 1s',
+		animationName: Radium.keyframes(fadeIn, 'fadeIn')
+	}
+}
 
 class Home extends Component {
   render() {
     return (
         <div className="main">
-          	<StyleRoot style={styles.fadeIn}>
-    			<div id="logo"><img src="img/logo.png"/></div>
-
-           		<div class="curved">    
-                	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="white" fill-opacity="1" d="M0,160L60,154.7C120,149,240,139,360,154.7C480,171,600,213,720,240C840,267,960,277,1080,261.3C1200,245,1320,203,1380,181.3L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
-                	<div class="photos"></div>
-            	</div>
+        	<StyleRoot style={styles.fadeIn}>
+	    		<div id="logo"><img src="img/logo.png"/></div>
+	    			<div className="curved">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="WHITE" fill-opacity="1" d="M0,256L80,266.7C160,277,320,299,480,282.7C640,267,800,213,960,197.3C1120,181,1280,203,1360,213.3L1440,224L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+						<div className="photos"></div>
+	    		</div>
     		</StyleRoot>
-        </div>
+
+    		<div id="t1Index">
+	    		<StyleRoot style={styles.fadeInDown}>
+	            <h1> Venha curtir momentos únicos e inesquecíveis com sua família e amigos!</h1>
+	            <h2>Saiba mais sobre o espaço!</h2>
+	            </StyleRoot>
+
+	        </div>
+       	</div>
     );
   }
 }
