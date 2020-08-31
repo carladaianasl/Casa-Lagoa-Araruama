112b0d251bfc640bf993c0fdec0a45eedddc4a22
carladaianasl <carladaianasl@gmail.com>
2020-08-31 16:37:11 -0300

    in development

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ class About extends Component {
     return (
         <div className="main">
         	<div className="sorry">
-        		<h2>Sorry for the inconvenience!</h2>
+        		<h2>In development...</h2>
 	          	<h3> We are working to give you a completely <b>new experience.</b></h3>
 
 	          	<div className="gear1"><img src="img/gear.png" width="180" name="gear1"/></div>
